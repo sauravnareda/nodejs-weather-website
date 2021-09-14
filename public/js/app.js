66e9d0907d84393b2e73c2818f384cd119cd76ec
Saurav Meena <saurav.nareda09@gmail.com>
2021-09-14 17:32:58 +0530

    Remove unnecessary console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Side JavaScript file is Loaded!');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
